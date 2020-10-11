58ecbaba608fb05a882513be132971bdee26013d
André Reigoto <andrereigoto@AndreMBP.netcabo.pt>
2020-10-12 00:48:34 +0100

    uninstall twek

--- scripts/uninstall_helper.sh
+++ scripts/uninstall_helper.sh
@@ -3,5 +3,6 @@ declare -a folders=( ~/Library /Library ~/Applications /Applications  )
 
 for f in ${folders[@]} ; do
     echo -e "\n\t$f"
-    find $f -d -iname $1
+    #sudo find $f -d -iname $1 2>&1 | grep -v "^find:"
+    sudo find $f -iname $1 2>&1 | grep -v "^find:"
 done
