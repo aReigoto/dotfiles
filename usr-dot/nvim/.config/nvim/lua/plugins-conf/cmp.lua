07519d295ad1d9e2bd5bfe6dbe49127838a156e3
aReigoto <18371208+aReigoto@users.noreply.github.com>
2023-02-04 10:46:45 +0000

    cmp Esc to cancel

--- usr-dot/nvim/.config/nvim/lua/plugins-conf/cmp.lua
+++ usr-dot/nvim/.config/nvim/lua/plugins-conf/cmp.lua
@@ -61,7 +61,8 @@ cmp.setup({
 		['<C-S-n>'] = cmp.mapping.scroll_docs(-4),
 		['<C-S-p>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
+		-- ['<C-e>'] = cmp.mapping.abort(),
+		['<Esc>'] = cmp.mapping.abort(),
 		['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
@@ -110,7 +111,8 @@ cmp.setup.cmdline("/", {
 cmp.setup.cmdline(':', {
   mapping = cmp.mapping.preset.cmdline(),
   sources = cmp.config.sources({
-    { name = 'path' }
+    -- { name = 'cmdline_history' },
+    { name = 'path' },
   }, {
     {
       name = 'cmdline',
