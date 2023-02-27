21725a16c3fdd7f384e64230949848ba31385ebd
aReigoto <18371208+aReigoto@users.noreply.github.com>
2023-02-27 21:08:15 +0000

    nvim-tree migrating to on attach bandage

--- usr-dot/nvim/.config/nvim/lua/plugins-conf/nvim-tree.lua
+++ usr-dot/nvim/.config/nvim/lua/plugins-conf/nvim-tree.lua
@@ -3,12 +3,14 @@ if not status_ok then
   return
 end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then
-  return
-end
+-- Check https://github.com/nvim-tree/nvim-tree.lua/wiki/Migrating-To-on_attach
+-- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+-- if not config_status_ok then
+--  print('status nvim_tree_config')
+--  return
+-- end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+-- local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
   update_focused_file = {
@@ -57,13 +59,13 @@ nvim_tree.setup {
     width = 30,
     -- height = 30,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
+    --mappings = {
+    --  list = {
+    --    { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+    --    { key = "h", cb = tree_cb "close_node" },
+    --    { key = "v", cb = tree_cb "vsplit" },
+    --  },
+    --},
   },
   git = {
     ignore = false,
