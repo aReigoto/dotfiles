87cc1241b22750e31a19edcf0afbb9462d511493
aReigoto <18371208+aReigoto@users.noreply.github.com>
2023-03-21 21:26:45 +0000

    tree-sitter-o2 from git

--- usr-dot/nvim/.config/nvim/lua/plugins-conf/treesitter.lua
+++ usr-dot/nvim/.config/nvim/lua/plugins-conf/treesitter.lua
@@ -157,12 +157,13 @@ configs.setup({
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.o2 = {
   install_info = {
-    url = "/Users/andre/Library/CloudStorage/OneDrive-LeicaCameraAG/00-Work/02-Programing/o2/tree-sitter-o2", -- local path or git repo
+    url = "https://github.com/aReigoto/tree-sitter-o2", -- local path or git repo
+    -- url = "/Users/andre/Library/CloudStorage/OneDrive-LeicaCameraAG/02-Personal/neovim-dev/tree-sitter-o2",
     files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
     -- optional entries:
-    -- branch = "main", -- default branch in case of git repo if different from master
+    branch = "main", -- default branch in case of git repo if different from master
     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+    requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
   },
   filetype = "o2", -- if filetype does not match the parser name
 }
