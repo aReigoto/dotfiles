bd09a57099e0f2bb5f70e42dbf3bc9c259a1f00d
André Reigoto <andrereigoto@andreMBP.lan>
2023-08-23 15:54:41 +0100

    State usage of LuaJit for lsp

--- usr-dot/nvim/.config/nvim/lua/lsp-conf/settings/lua_ls.lua
+++ usr-dot/nvim/.config/nvim/lua/lsp-conf/settings/lua_ls.lua
@@ -4,6 +4,11 @@ return {
 			diagnostics = {
 				globals = { "vim" },
 			},
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT'
+			},
+			-- Make the server aware of Neovim runtime files
 			workspace = {
 				library = {
 					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
