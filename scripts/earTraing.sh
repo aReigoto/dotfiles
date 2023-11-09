cbcf09b795580f40996ea536d96e4b50e592e4c0
andre <andre@andres-iMac.local>
2023-11-09 18:56:36 +0000

    ear

--- scripts/earTraing.sh
+++ scripts/earTraing.sh
@@ -10,4 +10,12 @@ if [[ $1 =~ '-e' ]] ; then
 fi
 
 pipenv run python earTraningChordsCmdL.py $@
-/Applications/Logic\ Pro\ X.app/Contents/MacOS/Logic\ Pro\ X Midi_files/earTraing.midi &
+/Applications/Logic\ Pro\ X.app/Contents/MacOS/Logic\ Pro\ X Midi_files/earTraining.midi &
+
+# earTraining.sh --n_chord_notes=3 --root_random='D3' --voice_leading=false
+# earTraining.sh --n_chord_notes=3 --root_random='D3' 
+
+#earTraining.sh --n_chord_notes=3 --root_random='D3' --voice_leading='{"0":1,"1":1}'
+#earTraining.sh --voice_leading='{"0":1,"1":1,"2":1}'
+#earTraining.sh  --voice_leading='{"0":1,"1":null,"2":1,"sum":2}'
+#earTraining.sh --voice_leading='{"0":1,"1":1,"sum":2}'
