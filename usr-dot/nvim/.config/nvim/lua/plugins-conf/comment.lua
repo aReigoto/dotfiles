95dc04647b7410f57c3d69e0c0e703820b399637
aReigoto <18371208+aReigoto@users.noreply.github.com>
2024-03-05 08:05:09 +0000

    refine update config

--- usr-dot/nvim/.config/nvim/lua/plugins-conf/comment.lua
+++ usr-dot/nvim/.config/nvim/lua/plugins-conf/comment.lua
@@ -3,7 +3,9 @@ if not status_ok then
   return
 end
 
-require('ts_context_commentstring').setup {}
+require('ts_context_commentstring').setup {
+  enable_autocmd = false,
+}
 
 comment.setup {
   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
