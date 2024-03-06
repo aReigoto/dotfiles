fbcff193c68b6ec3b13790e709f55592d5cd413f
aReigoto <18371208+aReigoto@users.noreply.github.com>
2024-03-06 11:38:51 +0000

    o2 TS move to comments branch

--- usr-dot/nvim/.config/nvim/lua/plugins-conf/treesitter.lua
+++ usr-dot/nvim/.config/nvim/lua/plugins-conf/treesitter.lua
@@ -160,7 +160,7 @@ parser_config.o2 = {
     url = "https://github.com/aReigoto/tree-sitter-o2", -- local path or git repo
     files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
     -- optional entries:
-    branch = "main", -- default branch in case of git repo if different from master
+    branch = "comments", -- default branch in case of git repo if different from master
     generate_requires_npm = false, -- if stand-alone parser without npm dependencies
     requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
   },
