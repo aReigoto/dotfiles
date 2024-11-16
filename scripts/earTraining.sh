2679511d39ba2eb8fcd08d99b8da7e32e2c15dc3
andrereigoto <andrereigoto@gmail.com>
2024-11-16 12:33:11 +0000

    call logicPro from python

--- scripts/earTraining.sh
+++ scripts/earTraining.sh
@@ -11,7 +11,17 @@ if [[ $1 =~ '-e' ]] ; then
 fi
 
 pipenv run python earTraningChordsCmdL.py $@
-/Applications/Logic\ Pro\ X.app/Contents/MacOS/Logic\ Pro\ X Midi_files/earTraining.midi &
+
+exit_code=$?
+
+if [ $exit_code -eq 0 ]; then
+    echo "Command succeeded"
+    #/Applications/Logic\ Pro\ X.app/Contents/MacOS/Logic\ Pro\ X Midi_files/earTraining.midi &
+else
+    echo "earTraningChordsCmdL.py failed with exit code $exit_code"
+fi
+
+
 
 # earTraining.sh --n_chord_notes=3 --root_random='D3' --voice_leading=false
 # earTraining.sh --n_chord_notes=3 --root_random='D3' 
