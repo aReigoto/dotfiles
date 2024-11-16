c2972af1e405214c2fedfddcbd8b201fe3bb358c
andrereigoto <andrereigoto@gmail.com>
2024-11-16 12:13:05 +0000

    earTraining typo

--- scripts/earTraining.sh
+++ scripts/earTraining.sh
@@ -5,8 +5,9 @@ echo -e "To edte the script run with -e flag."
 cd ~/Dropbox/00-NotSharing/02-Informatica/python/Projects/music/m21V2/
 
 if [[ $1 =~ '-e' ]] ; then
-    subl earTraningChordsCmdL.py
-    exit 0
+  code .  
+  #subl earTraningChordsCmdL.py
+  exit 0
 fi
 
 pipenv run python earTraningChordsCmdL.py $@
